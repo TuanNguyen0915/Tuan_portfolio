fd2d6d7833ed6be1aa70d23c95e12afe624b1ad1
Tuan Nguyen <tuan.nat915@gmail.com>
2023-11-22 17:49:00 -0500

    edit about imgs link

--- src/data/data.js
+++ src/data/data.js
@@ -1,18 +1,18 @@
 const skillsSet = [
     {
         title: 'Technologies',
-        imgLink: '/src/assets/images/code.gif',
+        imgLink: '../../assets/images/code.gif',
         
         items: ['HTML/CSS', 'JavaScript', 'React', 'Python', "MongoDb", 'Mongoose']
     },
     {
         title: 'Frameworks & Services',
-        imgLink: '/src/assets/images/gears.gif',
+        imgLink: '../../assets/images/gears.gif',
         items: ['Github', 'Google OAuth', 'JWT Authentication', 'JSON', 'Node.js', 'Express.js', 'Docker', 'Django']
     },
     {
         title: 'Methodologies',
-        imgLink: '/src/assets/images/learning.gif',
+        imgLink: '../../assets/images/learning.gif',
         items: ['Agile development', 'RESTful Routing', 'QA Testing', 'Problem Solving', 'Debugging']
     }
 ]
